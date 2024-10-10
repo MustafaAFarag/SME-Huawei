53bbd7323b77b5c685fb5ba5a3577852c906f4e8
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2024-10-10 12:46:55 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './styles/index.css';
+
+function App() {
+  return <div>Hello World Testing </div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Ensure to include .tsx and .jsx files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
