454582ebaa5e657e03790cad108873e4bd1d9025
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2024-10-16 22:44:24 +0300

    Test without Applayout.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './styles/index.css';
 import Homepage from './pages/Homepage';
 import Dashboard from './pages/Dashboard';
-import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
 
         <Route path="/" element={<Homepage />} />
       </Routes>
