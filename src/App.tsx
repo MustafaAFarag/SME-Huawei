b318612703663864a6789c06d45a2e7b66b50c4e
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2024-10-16 22:50:08 +0300

    Add AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './styles/index.css';
 import Homepage from './pages/Homepage';
 import Dashboard from './pages/Dashboard';
+import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<AppLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
 
         <Route path="/" element={<Homepage />} />
       </Routes>
